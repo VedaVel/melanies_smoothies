062166ad8f9eddf18fd3df4e07e3b33006ef5ac2
VedaVel <167787292+VedaVel@users.noreply.github.com>
2024-04-22 18:49:24 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,8 +13,9 @@ st.write(
 )
 #Enter Customer Name
 name_of_order = st.text_input('Name on Smoothie:', '')
-st.write('The name on your Smoothie will be:',name_of_order )
+st.write('The name on Smoothie will be:',name_of_order )
 st.write('hello')
+
 #Select Fruits from table
 cnx=st.connection("snowflake")
 session = cnx.session()
