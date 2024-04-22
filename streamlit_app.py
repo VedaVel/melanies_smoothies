f6e3e20b57a0d1d26f2678d6661a7eff2c92807d
VedaVel <167787292+VedaVel@users.noreply.github.com>
2024-04-22 17:59:16 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,12 +18,11 @@ st.write('The name on your Smoothie will be:',name_of_order )
 #Select Fruits from table
 cnx=st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#,col('SEARCH_ON'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 #Converting  snowpark dataframe into Pandas dataframe so we can use the LOC function
-#pd_df=my_dataframe.to_pandas()
+pd_df=my_dataframe.to_pandas()
 #st.dataframe(pd_df)
 #st.stop()
 
@@ -39,16 +38,18 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string +=fruit_chosen + ' '
-      #  search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-      #  st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
-       # st.subheader(fruit_chosen+' Nutritional Information')
+        st.subheader(fruit_chosen+' Nutritional Information')
         #New Section to display nutrition information
-       # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_chosen)
-        #fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_chosen)
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+     
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','""" + name_of_order +"""')"""
     time_to_insert = st.button('Submit')
+ 
     # This if statement executes when submit button has a value
     if time_to_insert:
     #if  ingredients_string: The if statement executes when ingredients_string has value
