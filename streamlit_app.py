74410dd25a0b566b74f49acfd333d1086f39567b
VedaVel <167787292+VedaVel@users.noreply.github.com>
2024-04-22 19:10:46 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ if ingredients_list:
 
         st.subheader(fruit_chosen+' Nutritional Information')
         #New Section to display nutrition information
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+search_on)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
      
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
