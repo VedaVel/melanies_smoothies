9f35086f9d37e9a69c542fdee3848fbecc700f3e
VedaVel <167787292+VedaVel@users.noreply.github.com>
2024-04-22 18:55:35 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,16 +14,14 @@ st.write(
 #Enter Customer Name
 name_of_order = st.text_input('Name on Smoothie:', '')
 st.write('The name on your Smoothie will be:',name_of_order )
-st.write('hello')
 
 #Select Fruits from table
 cnx=st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'),col('search_on'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
-st.write('hello')
 #Converting  snowpark dataframe into Pandas dataframe so we can use the LOC function
-#pd_df=my_dataframe.to_pandas()
+pd_df=my_dataframe.to_pandas()
 
 ingredients_list = st.multiselect(
     'Choose upto five fruits:'
@@ -37,13 +35,13 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string +=fruit_chosen + ' '
-        #search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
-        #st.subheader(fruit_chosen+' Nutritional Information')
+        st.subheader(fruit_chosen+' Nutritional Information')
         #New Section to display nutrition information
-        #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_chosen)
-        #fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_chosen)
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
      
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','""" + name_of_order +"""')"""
