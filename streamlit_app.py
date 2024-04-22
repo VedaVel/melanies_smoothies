aebe512d86ddc43cf62e8788573281af44c97578
VedaVel <167787292+VedaVel@users.noreply.github.com>
2024-04-22 15:34:49 -0400

    Update streamlit_app.py
    Added comments about new section

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered '+ name_of_order+ '!', icon="✅")
 
-
+#New Section to display nutrition information
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 st.text(fruityvice_response)
 
